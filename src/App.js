3171901db143ca984e6c1af5ea10e1a92fc64e25
Fabricio Alves <123993244+fabricioalvess@users.noreply.github.com>
2023-06-22 14:21:16 -0300

    componentizando o header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Logo from './componentes/Logo';
+import perfilSvg from './imagens/perfil.svg'
+import sacolaSvg from './imagens/sacola.svg'
+const icones = [perfilSvg, sacolaSvg]
+const textoOpcao = ['CATEGORIA','FAVORITOS','MINHA ESTANTE']
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <Logo></Logo>
+        <ul className='opcoes'>
+          {textoOpcao.map((texto)=>(
+            <li className='opcao'><p>{texto}</p></li>))}
+        </ul>
+        <ul className='icones'>
+            {
+              icones.map((icone)=>(
+                <li className='icone'><img src={icone}></img></li>
+              ))
+            }
+        </ul>
       </header>
     </div>
+
   );
 }
-
-export default App;
+export default App
