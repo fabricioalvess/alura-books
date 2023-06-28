9b2cd268ea2bce510546dbc2de183352b006d4d7
Fabricio Alves <123993244+fabricioalvess@users.noreply.github.com>
2023-06-27 21:07:42 -0300

    Criou o filtro de livros com useState

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Input from "../Input"
 import styled from "styled-components"
+import {livros} from './dadosPesquisa'
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg,#002f52 35%, #326589 165%);
@@ -21,17 +22,49 @@ const SubTitulo = styled.h3`
     font-weight: 500;
     margin-bottom: 40px;
 `
+const Resultado = styled.div`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    margin-bottom:20px;
+    cursor:pointer;
+        p{
+            width:200px;
+        }
+        img{
+            width:100px;
+        }
+        &:hover{
+            border:1px solid white;
+        }
+`
 export default function Pesquisa(){
-    const [textoDigitado, setTextoDigitado] = useState('')
+    const [livrosPesquisados, setLivrosPesquisados] = useState([])
+
     return(
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <SubTitulo>Encontre seu livro em nossa estante.</SubTitulo>
             <Input
                 placeholder="Escreva sua proxima leitura."
-                onBlur={(evento)=>setTextoDigitado(evento.target.value)}
+                onBlur={(evento)=>{
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = livros.filter(
+                        livro => livro.nome.includes(textoDigitado)
+                    )
+                    setLivrosPesquisados(resultadoPesquisa)
+
+                }} 
             />
-            <p>{textoDigitado}</p>
+          {
+            livrosPesquisados.map( livro =>(
+                <Resultado>
+                    <img src={livro.src} alt='livros'></img>
+                    <p>{livro.nome}</p>
+                </Resultado>
+            ))
+          }
         </PesquisaContainer>
     )
 }
+
